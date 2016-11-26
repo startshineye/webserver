17252f8317ab2781170b6e5b6d6f41483789db20
startshineye <13001955858@163.com>
2016-11-26 20:07:32 +0800

    webserver-01

---
+++ webserver-01/src/com/yxm/WebServer.java
@@ -0,0 +1,33 @@
+package com.yxm;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+	// 写一个服务器启动方法
+	public void serverStart(int port) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(80);// 服务器端监听80端口
+			while (true) {
+				Socket socket = serverSocket.accept();// 等待连接
+				new Processor(socket).start();// 在processor中处理,processor是多线程的
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}// 服务器端监听80端口
+
+	}
+
+	public static void main(String[] args) {
+		/**main方法用于生成當前服務實例并執行start方法
+		 * 默认是80端口号，如果是通过参数传进来的就是传进来的端口号
+		 */
+		// 通过命令行获取新的端口号
+		int port = 80;
+		if (args.length == 1) {
+			port = Integer.parseInt(args[0]);
+		}
+		// 生成当前webserver实例执行start方法
+		new WebServer().serverStart(port);
+	}
+}
